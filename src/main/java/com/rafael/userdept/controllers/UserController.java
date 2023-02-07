71968daebe9a17497088502ff81a71a5346d4a3d
Rafael Almeida <rafael.profeng@gmail.com>
2023-02-06 21:10:35 -0300

    feat: Add get users endpoint

---
+++ src/main/java/com/rafael/userdept/controllers/UserController.java
@@ -0,0 +1,24 @@
+package com.rafael.userdept.controllers;
+
+import com.rafael.userdept.entities.User;
+import com.rafael.userdept.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping
+    public List<User> findAll() {
+        return repository.findAll();
+
+    }
+}
---
+++ src/main/java/com/rafael/userdept/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.rafael.userdept.repositories;
+
+import com.rafael.userdept.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
